9c6d6adfa1a04e01a1efc2ac12bd276816f299c1
Vadym <tunik.vadym@gmail.com>
2024-10-20 17:15:24 +0300

    fav sol

--- richest-customer-wealth.py
+++ richest-customer-wealth.py
@@ -1,15 +1,16 @@
 # PROBLEM DESCRIPTON: https://leetcode.com/problems/richest-customer-wealth/description/
 # SOLUTION SUMBITION WITH NUMPY: https://leetcode.com/problems/richest-customer-wealth/submissions/1428397229/
 # SOLUTION WITH PURE PYTHON: https://leetcode.com/problems/richest-customer-wealth/submissions/1428399266/
+# SOLUTION WITHOUT ANY FUNCTIONS: https://leetcode.com/problems/richest-customer-wealth/submissions/1428404078/
 from typing import List
-# import numpy as np 
+import numpy as np 
 
-# # SOLUTION WITH NUMPY - TOP 0.61%
-# class Solution:
-#     def maximumWealth(self, accounts: List[List[int]]) -> int:
-#         accounts_wealth_per_customer = np.sum(accounts, axis=1)
-#         accounts_max_wealth  = np.max(accounts_wealth_per_customer)
-#         return int(accounts_max_wealth)
+# SOLUTION WITH NUMPY - TOP 0.61%
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        accounts_wealth_per_customer = np.sum(accounts, axis=1)
+        accounts_max_wealth  = np.max(accounts_wealth_per_customer)
+        return int(accounts_max_wealth)
 
 # # SOLUTION WITH PURE PYTHON - TOP 0.61%
 # class Solution:
@@ -18,22 +19,22 @@ from typing import List
 #         accounts_max_wealth = max(accounts_wealth_per_customer)
 #         return accounts_max_wealth
     
-# SOLUTION WITHOUT ANY FUNCTIONS - TOP 0.61%
-class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        accounts_wealth_per_customer = []
-        for acc in accounts:
-            wealth = 0
-            for bank_money in acc:
-                wealth += bank_money
-            accounts_wealth_per_customer.append(wealth)
+# # SOLUTION WITHOUT ANY FUNCTIONS - TOP 0.61%
+# class Solution:
+#     def maximumWealth(self, accounts: List[List[int]]) -> int:
+#         accounts_wealth_per_customer = []
+#         for acc in accounts:
+#             wealth = 0
+#             for bank_money in acc:
+#                 wealth += bank_money
+#             accounts_wealth_per_customer.append(wealth)
 
-        max_wealth = accounts_wealth_per_customer[0]
-        for wealth in accounts_wealth_per_customer:
-            if wealth > max_wealth:
-                max_wealth = wealth
+#         max_wealth = accounts_wealth_per_customer[0]
+#         for wealth in accounts_wealth_per_customer:
+#             if wealth > max_wealth:
+#                 max_wealth = wealth
         
-        return max_wealth
+#         return max_wealth
 
 def test1():
     accounts = [[1,2,3],[3,2,1]]
